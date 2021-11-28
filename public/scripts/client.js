ce9e9d5a2c0003e79a78a714561b2e4187dcef13
brogr <dev@brogramming.com>
2021-11-29 00:59:56 +0100

    implemented css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(function () {
+  document.querySelectorAll(".tweet time").forEach((timeEl) => {
+    timeEl.textContent = timeago.format(timeEl.textContent);
+  });
+});
