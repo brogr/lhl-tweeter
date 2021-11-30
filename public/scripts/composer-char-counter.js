7709139fbceb52a6b5b11c5383e1bbf9bbf033d8
Fabian Voegeli <dev@brogramming.com>
2021-11-30 16:20:17 +0000

    implemented character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(function () {
+  const maxCharacters = 140;
+  $("#tweet-text").on("input", function (event) {
+    // calculate remaining characters, set counter
+    let remainingCharacters = maxCharacters - $(this).val().length;
+    const $counter = $(this).parents("form").find(".counter");
+    $counter.text(remainingCharacters);
+    // indicate when maxCharacters reached
+    if (remainingCharacters < 0) {
+      $counter.addClass("warn")
+    } else {
+      $counter.removeClass("warn");
+    }
+  });
+});
