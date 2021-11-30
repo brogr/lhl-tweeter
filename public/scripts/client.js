c65563ba32bf21591c8fc7619b4965095c454201
Fabian Voegeli <dev@brogramming.com>
2021-11-30 16:46:05 +0000

    implemented basic version of renderTweets()

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,28 +11,44 @@ $(function () {
   }); 
   */
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
     },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
     },
-    created_at: 1461116232227,
-  };
-
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet.html()); // to see what it looks like
-  $("#tweets").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  ];
+  renderTweets(data);
 });
 
+// renderTweets: takes in an array of tweet objects, sends each to createTweetElement() and appends it to the #tweets container
+const renderTweets = function (tweetsData) {
+  tweetsData.forEach((tweetData) => {
+    const $tweet = createTweetElement(tweetData);
+    $("#tweets").append($tweet); // add it to the DOM
+  });
+};
+
 // createTweetElement: takes in a tweet object and returns a tweet <article> element containing the entire HTML structure of the tweet
-const createTweetElement = function (tweetData) {
+const createTweetElement = function(tweetData) {
   const $tweet = $(`
     <article class="tweet">
       <header>
